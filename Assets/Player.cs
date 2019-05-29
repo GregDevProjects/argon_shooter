a5c491343d8c2b99e706cc3b3bbf622a4326f34c
GregDevProjects <gdonaldsonmclean@gmail.com>
2019-05-29 13:40:48 -0300

    about to start messing around

---
+++ Assets/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float xClamp = 1f;
+    [SerializeField] float yClamp = 1f;
+    [Tooltip("In ms^-1")][SerializeField] float speed = 4f;
+
+    Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mainCamera = GetComponentInParent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        //if the frame takes longer, you want to move further
+        //this is why we multiply by deltatime
+        float xOffset = speed * xThrow * Time.deltaTime;
+        float rawNewXPos = Mathf.Clamp(
+            transform.localPosition.x + xOffset,
+            -xClamp,
+            xClamp
+            );
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = speed * yThrow * Time.deltaTime;
+
+        float rawNewYPos = transform.localPosition.y + yOffset;
+
+        Mathf.Clamp(
+            transform.localPosition.y + yOffset,
+            -yClamp,
+            yClamp
+            );
+
+        transform.localPosition = mainCamera.ViewportToWorldPoint(new Vector3(rawNewXPos, rawNewYPos, transform.localPosition.z));
+        print(xThrow);
+    }
+}
